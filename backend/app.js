0abef5f5da7e1a8252a25f6054f18fc5734df698
Daniil Yefimov <danyefimoff@gmail.com>
2023-02-16 03:57:08 +0300

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -20,8 +20,6 @@ mongoose.connect('mongodb://127.0.0.1:27017/mestodb', {
 });
 app.use(express.json());
 
-app.use('/api', require('../router'));
-
 app.use(cors);
 
 app.use(requestLogger); // подключаем логгер запросов
