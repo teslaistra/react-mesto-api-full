f40b3b425e4e4fcf2e2c58302184a5714b25da82
Daniil Yefimov <danyefimoff@gmail.com>
2023-02-19 00:32:25 +0300

    е

--- backend/controllers/cards.js
+++ backend/controllers/cards.js
@@ -60,7 +60,7 @@ module.exports.likeCard = (req, res, next) => {
     { new: true },
   ).then((card) => {
     if (card) {
-      res.send({ data: card });
+      res.send({ card });
     } else {
       throw new NotFoundError('Запрашиваемая карточка не найдена');
     }
